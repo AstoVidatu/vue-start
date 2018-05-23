0bad97a0227f79a9fddacc69d19803666ff22c66
AstoVidatu <stefanjankovic31@gmail.com>
2018-05-24 01:01:52 +0200

    Added API and connection between client and server

--- server/src/app.js
+++ server/src/app.js
@@ -8,9 +8,9 @@ app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/status', (req, res) => {
+app.post('/register', (req, res) => {
   res.send({
-    message: 'Zdravo'
+    message: `Pozdrav ${req.body.email}! Your user was registered!`
   })
 })
 
