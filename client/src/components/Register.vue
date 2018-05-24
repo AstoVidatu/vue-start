aa757959c093bc9c15375837fce5a888fbbdf49f
AstoVidatu <stefanjankovic31@gmail.com>
2018-05-24 03:41:20 +0200

    UI elements added for register page

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,24 +1,34 @@
 <template>
-  <div>
-    <input
-      type="text"
-      name="email"
-      v-model="email"
-      placeholder="email"
-    />
-    <br>
-    <input
-      type="password"
-      name="password"
-      v-model="password"
-      placeholder="password"
-    />
-    <br>
-    <button
-    @click="register">
-      Register
-    </button>
-  </div>
+  <v-layout column class="center">
+    <v-flex xs6 class="width" >
+      <panel title="Register">
+        <form
+          name="tab-tracker-form"
+          autocomplete="off">
+          <v-text-field
+            label="Email"
+            v-model="email"
+          ></v-text-field>
+          <br>
+          <v-text-field
+            label="Password"
+            type="password"
+            v-model="password"
+            autocomplete="new-password"
+          ></v-text-field>
+        </form>
+        <br>
+        <div class="danger-alert" v-html="error" />
+        <br>
+        <v-btn
+          dark
+          class="cyan"
+          @click="register">
+          Register
+        </v-btn>
+      </panel>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
@@ -27,20 +37,30 @@ export default {
   data () {
     return {
       email: '',
-      password: ''
+      password: '',
+      error: null
     }
   },
   methods: {
     async register () {
-      const response = await AuthService.register({
-        email: this.email,
-        password: this.password
-      })
-      console.log(response.data)
+      try {
+        await AuthService.register({
+          email: this.email,
+          password: this.password
+        })
+      } catch (error) {
+        this.error = error.response.data.error
+      }
     }
   }
 }
 </script>
-
 <style scoped>
+.width{
+  width: 50%;
+}
+.center{
+  margin-left: auto;
+  margin-right: auto;
+}
 </style>
